b49e68799f9a268dbc827771969a55ab047a0800
BRAUN REMI <remi.braun@unistra.fr>
2023-01-02 12:16:18 +0100

    Add misc files

---
+++ eopairs/__init__.py
@@ -0,0 +1,12 @@
+
+from .__meta__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __documentation__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+
+import setuptools
+
+from eopairs.__meta__ import (
+    __author__,
+    __author_email__,
+    __description__,
+    __documentation__,
+    __title__,
+    __url__,
+    __version__,
+)
+
+BASEDIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+with open(os.path.join(BASEDIR, "README.md"), "r", encoding="utf8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "eoreader>=0.18.1",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+    project_urls={
+        "Bug Tracker": f"{__url__}/issues/",
+        "Documentation": __documentation__,
+        "Source Code": __url__,
+    },
+)
