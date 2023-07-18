5d5259adba508be2dea11ba501d4b8e92f7f13ad
BRAUN REMI <remi.braun@unistra.fr>
2023-07-18 14:38:43 +0200

    Going opensource

--- eosets/__meta__.py
+++ eosets/__meta__.py
@@ -1,12 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023, SERTIT-ICube - France, https://sertit.unistra.fr/
+# This file is part of eosets project
+#     https://github.com/sertit/eosets
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 **EOSets** library
 """
-__version__ = "0.0.0.dev0"
+__version__ = "0.1.0"
 __title__ = "eosets"
-__description__ = "This library aims to simplify any process working with pairs of EO data handled by EOReader."
+__description__ = "This library aims to simplify any process working with different sets of EO data handled by EOReader."
 __author__ = "ICube-SERTIT"
 __author_email__ = "dev-sertit@unistra.fr"
-__url__ = "https://git.unistra.fr/sertit/libraries/eosets"
+__url__ = "https://github.com/sertit/eosets"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2023, SERTIT-ICube - France, https://sertit.unistra.fr/"
 __documentation__ = "https://sertit.pages.unistra.fr/libraries/eosets"
