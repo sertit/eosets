368b4e5651d73f3d5a4b946a9462ca88fce0df2e
BRAUN REMI <remi.braun@unistra.fr>
2025-07-09 12:13:24 +0200

    FIX CI

--- ci/test_pair.py
+++ ci/test_pair.py
@@ -170,10 +170,8 @@ def test_l8_pair(tmp_path):
     """Test pair object with Landsat-8 products"""
     # Test paths without a list
     l8_paths = {
-        "reference_paths": data_folder()
-        / "LC08_L1TP_202032_20200828_20200906_02_T1.tar",
-        "secondary_paths": data_folder()
-        / "LC08_L1TP_202032_20200929_20201006_02_T1.tar",
+        "reference_paths": data_folder() / "LC08_L1TP_202032_20200828_20200906_02_T1",
+        "secondary_paths": data_folder() / "LC08_L1TP_202032_20200929_20201006_02_T1",
     }
     _test_pair_core(l8_paths, tmp_path)
 
